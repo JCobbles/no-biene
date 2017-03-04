143eaf7891825bc86d52dcadb2bb65153500a952
dmcmillan90 <doug90@gmail.com>
2017-03-04 15:08:00 +0100

    attempted ngrepeat

---
+++ public/controllers/fundingOption.js
@@ -0,0 +1,25 @@
+// functions for a funding option
+
+function getCost(option) {
+
+}
+
+function setCost(option) {
+
+}
+
+function getDescription(option) {
+
+}
+
+function setDescription(option) {
+
+}
+
+function getOwner(option) {
+
+}
+
+function setOwner(option) {
+
+}
