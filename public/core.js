03b71d9ac8acaffa56a37d85552804c383ed7ca7
Jacob Moss <cob.mossy@gmail.com>
2017-03-04 03:14:37 +0000

    add angular routing

--- public/core.js
+++ public/core.js
@@ -1,4 +1,4 @@
-var scotchTodo = angular.module('scotchTodo', []);
+var scotchTodo = angular.module('scotchTodo', ['app', 'ng']);
 
 function mainController($scope, $http) {
     $scope.formData = {};
